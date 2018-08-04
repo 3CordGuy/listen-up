433e03f5d73b9ed9f4306b0a708139873fd676ec
Josh Weaver <jweaver@ministrysync.com>
2018-08-03 22:22:15 -0500

    spiffy up some styling

--- src/services/posts/posts.hooks.js
+++ src/services/posts/posts.hooks.js
@@ -1,8 +1,8 @@
-
+const { disallow } = require('feathers-hooks-common');
 
 module.exports = {
   before: {
-    all: [],
+    all: [disallow('external')],
     find: [],
     get: [],
     create: [],
