b0f6a798379661d1f752058ea9a7f1fe773731a2
Josh Weaver <jweaver@ministrysync.com>
2018-10-01 20:43:24 -0500

    mitigate one deprecation warning for another

--- src/mongoose.js
+++ src/mongoose.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
     app.get('mongodb'),
     { useNewUrlParser: true }
   )
+  mongoose.set('useCreateIndex', true)
   mongoose.Promise = global.Promise
 
   app.set('mongooseClient', mongoose)
