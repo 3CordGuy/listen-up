81b4e2759fc516e7d81de18ca14e19d0a3522c1c
Josh Weaver <jweaver911@gmail.com>
2021-10-14 00:06:55 +0000

    ports over some initial layout code

---
+++ src/layout/main.svelte
@@ -0,0 +1,46 @@
+<script>
+	// logic goes here
+</script>
+
+<div class="flex">
+  <div class="w-full sm:w-1/3 bg-indigo-900 md:h-screen flex flex-col border-r">
+    <div class="w-full flex-grow flex flex-col justify-end text-white text-center">
+      <div>
+        <h1 class="font-bold text-5xl">
+          <i class="fas fa-headphones"></i>&nbsp;Listen Up
+        </h1>
+        <h2 class="font-thin text-2xl">Super Simple Webhook Testing</h2>
+      </div>
+
+      <div class="mt-4" v-if="!showListener">
+        <button
+          class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          >Create New Hook
+          Endpoint
+        </button>
+      </div>
+    </div>
+    <div class="w-full flex justify-center flex-grow text-white text-center font-hairline">
+      <div class="footer flex flex-row content-center self-end pb-4">
+        <a class="mr-2" href="//github.com/3Cordguy/listen-up">
+          <span>
+            <i class="fab fa-2x fa-github-square"></i>
+          </span>
+        </a>
+        <a class="mr-2" href="//www.twitter.com/3CordGuy">
+          <span>
+            <i class="fab fa-2x fa-twitter"></i>
+          </span>
+        </a>
+        <div>
+          Listen Up &copy; 2021 Josh Weaver
+        </div>
+      </div>
+    </div>
+  </div>
+  <slot name="body"></slot>
+</div>
+
+<style>
+	/* styles go here */
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Layout from "../layout/main.svelte";
+</script>
+
+<svelte:head>
+	<title>Listen Up | Super Simple Webhook Testing</title>
+  <meta name="description"
+    content="Test your webhooks by using a simple generated endpoint and see your HTTP request come through in realtime." />
+</svelte:head>
+
+<Layout>
+
+  <div slot="body" class="w-2/3 bg-gray-100 h-screen flex flex-col p-24 justify-center items-center" >
+    <div class="bg-indigo-200 p-4 rounded">
+      <h1 class="text-xl font-bold mb-2">How do I use this?</h1>
+      <p>Create a webhook endpoint, then make an HTTP <strong>POST</strong> request to it. We will catch the data. <small><em>Don't worry. Your data doesn't persist</em></small>.</p>
+      <img class="mt-4" src="./listen-up-screen.png" alt="listen-up realtime webhook tool">
+      <h1 class="text-xl font-bold mt-4 mb-2">What is this good for?</h1>
+      <p>Listen Up can be a tool for testing if your webhook/web callback or server is sending information properly. <strong>In other words, it catches data you throw at it</strong>. This data is displayed in realtime using websockets and easily visible.</p>
+      <p class="mt-4">No need to hunt for data in a database to see if the proper data is coming across.</p>
+      <p class="mt-4"><em>Give it a try!</em></p>
+    </div>
+  <!--/#app-->
+</div>
+</Layout>
